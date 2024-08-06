a6308b6399f73f605f2f6d2be2f12d78155f9800
wsm25 <wsm_25@qq.com>
2024-08-06 14:58:11 +0800

    feat: give up bath

--- bath-status/src/main.rs
+++ bath-status/src/main.rs
@@ -1,8 +1,29 @@
 mod database;
 mod webapi;
-mod rpc;
+use std::time::SystemTime;
 
+#[deprecated = "dead"]
 fn main(){
+    let conn=rusqlite::Connection::open("1.db").unwrap();
+    let mut db=database::DataBase::new(&conn).unwrap();
+    const LOOP:u32=10000;
+    let begin=SystemTime::now();
+    for i in 0..LOOP{
+        db.ins_bathtrunk(
+            &database::BathTrunk { building: i.to_string(), cord: (0, 0) }
+        ).unwrap();
+    }
+    let end=SystemTime::now();
+    println!("insert: {:?}/op", end.duration_since(begin).unwrap()/LOOP);
+    for i in 0..LOOP{
+        db.query_bathtrunks(&i.to_string()).unwrap().count();
+    }
+    let end=SystemTime::now();
+    println!("select: {:?}/op", end.duration_since(begin).unwrap()/LOOP);
+    
+}
+
+fn _main(){
     std::thread::spawn(||{
         tokio::runtime::Builder::new_current_thread()
             .enable_io()
